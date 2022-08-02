9174598e3d2f085a6916ea0bab23508ee190a723
AndrewG76 <89551299+AndrewG76@users.noreply.github.com>
2022-08-01 17:51:08 -0700

    Add files via upload

---
+++ Macro for Microsoft Rewards.py
@@ -0,0 +1,11 @@
+import pyautogui #used to track mouse movements
+import time #used to call time commands and delay stuff
+
+print("yo wassup")
+#userInput = input("give me some kind of user input")
+time.sleep(2)
+print("after 2 seconds")
+
+#print(pyautogui.position())
+
+pyautogui.click(1376,2104)
