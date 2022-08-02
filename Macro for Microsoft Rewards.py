c6c91cbad5fde676a41355c7e6256fdec2f694bb
Andrew Gravino <aggravino@cpp.edu>
2022-08-02 11:08:50 -0700

    adding some extra bits regarding macros

--- Macro for Microsoft Rewards.py
+++ Macro for Microsoft Rewards.py
@@ -8,4 +8,11 @@ print("after 2 seconds")
 
 #print(pyautogui.position())
 
-pyautogui.click(1376,2104)
+pyautogui.click(685,1045) #this will click my file explorer on the vertical monitor setup
+
+time.sleep(5)
+
+#maybe i can use like random function to choose between letters a-z for a bing search
+#have it automate pressing the start button and typing in microsoft edge
+#then it can macro press like ctrl e and then start typing stuff to search and then ctrl e again and keeps looping
+
