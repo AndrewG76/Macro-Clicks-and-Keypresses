d92e0a15f1dffd4326b16b7d12861416a465e578
Andrew Gravino <aggravino@cpp.edu>
2022-08-02 18:13:29 -0700

    added 30 search function

--- Macro for Microsoft Rewards.py
+++ Macro for Microsoft Rewards.py
@@ -1,16 +1,68 @@
 import pyautogui #used to track mouse movements
 import time #used to call time commands and delay stuff
+import string #this will be used for just choosing letters
+import random #this will be used for randomly selecting letters
+import sys
+
+numOfDownPresses = [1, 2, 3, 4, 5, 6]
 
 print("yo wassup")
 #userInput = input("give me some kind of user input")
 time.sleep(2)
 print("after 2 seconds")
 
-#print(pyautogui.position())
+heldPosition = pyautogui.position()
+print(heldPosition)
 
-pyautogui.click(685,1045) #this will click my file explorer on the vertical monitor setup
+#pyautogui.click(685,1045) #this will click my file explorer on the vertical monitor setup
 
-time.sleep(5)
+#pyautogui.click(612, 1049) #this will click microsoft edge on the vertical monitor setup
+
+
+promptChoice = input("look at the current position, is that where you want it to start? (Y/N)")
+
+if promptChoice == "Y" or promptChoice == "y":
+    pyautogui.click(heldPosition)
+else:
+    sys.exit()
+
+time.sleep(5) #wait for edge to open up
+
+#then press backspace, then any key on the alphabet, then another random key on the alphabet, then press down twice, then press enter
+
+for i in range(0, 30):
+    pyautogui.hotkey('ctrl', 'e')  #ctrl e opens the search bar
+    
+    #equivalent method is do the hotkey by simulating pressing the buttons down
+    #with pyautogui.hold('ctrl'): #press the ctrl key down
+    #    pyautogui.press('e') 
+
+    pyautogui.press('backspace')
+
+    time.sleep(1) #wait a second
+    
+    randomLetter = random.choice(string.ascii_letters) #choose a random letter
+    pyautogui.press(randomLetter) #type the random letter
+    
+    time.sleep(random.random()*3) #wait a little but in random times to avoid being spotted
+    
+    randomLetter = random.choice(string.ascii_letters) #again
+    pyautogui.press(randomLetter)
+    
+    for i in range(0, random.choice(numOfDownPresses)): #avoiding bot detection for like how many search results down we go
+        pyautogui.press('down')
+        time.sleep(random.random())
+        
+    pyautogui.press('enter')
+    
+    time.sleep(random.random()*5)
+        
+    
+    
+    
+    
+    
+    
 
 #maybe i can use like random function to choose between letters a-z for a bing search
 #have it automate pressing the start button and typing in microsoft edge
